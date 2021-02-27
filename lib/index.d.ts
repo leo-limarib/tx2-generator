047384f5d19934a3ecfe5ff33f0a5febca7afa16
Leonardo Ribeiro <leolimaribe@gmail.com>
2021-02-27 09:11:12 -0300

    updated documentation on dadosNota

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,8 +3,16 @@ import DadosNota from './interfaces/dadosNota.interface';
  * Gera o arquivo tx2 no caminho especificado.
  * @param caminhoTx2 o caminho onde o tx2 será gerado (um arquivo com o mesmo nome não pode existir)
  * @param dadosNota um objeto contendo os dados iniciais da nota.
- * @param dadosEmitente um objeto contendo os dados do emitente.
- * @param dadosItem um objeto contendo os dados do item.
+ * @param dadosNota.Id_A03 Não precisa ser preenchido.
+ * @param dadosNota.versao_A02 Versão.
+ * @param dadosNota.cUF_B02 UF do emitente.
+ * @param dadosNota.cNF_B03 Código gerado pelo sistema para nosso controle, até 8 digitos.
+ * @param dadosNota.natOp_B04
+ * @param dadosNota.mod_B06 Sempre 65 nessa função. 65 = NFCE.
+ * @param dadosNota.serie_B07 Cada operador do caixa vai ter sua série.
+ * @param dadosNota.nNF_B08 Controle da numeração pelo SaaS.
+ * @param dadosNota.dhEmi_B09 Data (deve seguir formato usado pelo tx2)
+ * @param dadosNota.tpAmb_B24 Ambiente produção ou homologação.
  * @return retorna uma string do caminho onde o arquivo foi gerado
  */
 export declare const generateNfce: (caminhoTx2: string, dadosNota: DadosNota) => Promise<String>;
